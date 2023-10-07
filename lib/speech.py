b3aef67a67f2e04e26d2ad5e0ea5fe18e56665a3
the_byte_bender <cccefg2@gmail.com>
2023-10-08 01:39:54 +0300

    refactor: Fix formatting

--- lib/speech.py
+++ lib/speech.py
@@ -1,6 +1,9 @@
 from cytolk import tolk
+
 tolk.try_sapi(True)
 tolk.load("__compiled__" not in globals())  # For compatibility with nuitka
-def speak(text: str, interupt = True):
+
+
+def speak(text: str, interupt=True):
     """Speak [text]. [interupt] specifies whether any currently spoken text should be interupted"""
     tolk.output(text, interupt)
