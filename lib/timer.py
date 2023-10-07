fbf22be9df7edda8cbd5330631c1c9f3eacf0f7c
the_byte_bender <cccefg2@gmail.com>
2023-10-08 00:05:28 +0300

    feat: Add a timer class

---
+++ lib/timer.py
@@ -0,0 +1,26 @@
+from wx import StopWatch
+
+
+class Timer:
+    """A timer."""
+
+    def __init__(self):
+        self.w = StopWatch()
+
+    @property
+    def elapsed(self) -> int:
+        """The elapsed time in milliseconds."""
+        return self.w.Time()
+
+    @elapsed.setter
+    def set_elapsed(self, ms: int):
+        self.w.Start(ms)
+
+    def restart(self):
+        self.elapsed = 0
+
+    def pause(self):
+        self.w.Pause()
+
+    def resume(self):
+        self.w.Resume()
