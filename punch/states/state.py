4a87a3863fa2f62c4cdc265e6c7918081d02d108
the_byte_bender <cccefg2@gmail.com>
2023-10-09 22:56:38 +0300

    feat: Rename on_enter to on_push and add a new on_enter and on_exit() methods for when the state becomes on top of the stack.

--- punch/states/state.py
+++ punch/states/state.py
@@ -9,15 +9,25 @@ class State:
         self.parent = None
         self.stack = []
 
-    def on_enter(self):
-        """Called when this state was entered. Initialize resources here, and call the super classes enter() too."""
+    def on_push(self):
+        """Called when this state is pushed on to the parrent state's stack. Initialize resources here, and call the super classes on_push() too."""
         for state in self.stack:
-            state.enter()
+            state.on_push()
 
     def on_pop(self):
         """Called after this state is popped off the parrent state's stack. Destroy any resources asociated with this state and call the super classes pop() method"""
         for state in self.stack:
             state.on_pop()
+
+    def on_enter(self):
+        """Called once right after on_push(), then is called when this state becomes on top of the stack. If you overwride this make sure to call the super class's on_enter() as well."""
+        for state in self.stack:
+            state.on_enter()
+
+    def on_exit(self):
+        """Called once before on_pop(), and called every time a state is pushed on top of this. If you override this make sure to call the super class's on_exit()"""
+        for state in self.stack:
+            state.on_exit()
 
     def update(self, key_events: list[KeyEvent]) -> None:
         """Called on each tick of the game loop. key_events contains a list of key events since the last frame. Call the super classes update() method if you overwride this method"""
@@ -27,16 +37,22 @@ class State:
 
     def append(self, state):
         """Append a state to this states stack. Returns the state that was appended"""
+        if self.stack:
+            self.stack[-1].on_exit()
         self.stack.append(state)
         state.parent = self
+        state.on_push()
         state.on_enter()
         return state
 
     def pop(self):
         """pop the last state from this state's stack. Raise IndexError if empty."""
         state = self.stack.pop()
+        state.on_exit()
         state.on_pop()
         state.parent = None
+        if self.stack:
+            self.stack[-1].on_enter()
         return state
 
     def replace_last_substate(self, state):
