93c910d8c8c6edba4954ca616d5bbaa3b534a965
the_byte_bender <cccefg2@gmail.com>
2023-10-10 23:12:51 +0300

    feat(gameplay): Initialize the world map when gameplay is pushed

--- punch/states/gameplay.py
+++ punch/states/gameplay.py
@@ -1,14 +1,20 @@
 import wx
 from ..key_event import KeyEvent
 from ..consts import EVT_KEY_DOWN, EVT_KEY_UP
+from ..world_map import WorldMap, TileType
 from .state import State
 
 
 class Gameplay(State):
     def on_push(self):
         super().on_push()
+        self.initialize_map()
         game = self.get_root_state()
-        self.ambience = game.audio.new_sound("ambience.ogg", looping=True, direct = True)
+        self.ambience = game.audio.new_sound("ambience.ogg", looping=True, direct=True)
+
+    def initialize_map(self):
+        self.world_map = WorldMap(3, 10, 1)
+        self.world_map.set_tiles_in(0, 2, 0, 9, 0, 0, TileType.rocks)
 
     def on_enter(self):
         super().on_enter()
@@ -26,6 +32,5 @@ class Gameplay(State):
         super().update(key_events)
         for event in key_events:
             if event.type == EVT_KEY_DOWN:
-                if event.key_code == wx.WXK_ESCAPE:
-                    if self.parent:
-                        self.parent.pop()
+                if event.key_code == wx.WXK_ESCAPE and self.parent:
+                    self.parent.pop()
